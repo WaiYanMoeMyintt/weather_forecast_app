0d2a4a379a0f53c53547cfabc84b496a8f12547a
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-05-13 16:15:25 +0630

    Add progress and skeleton ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Navbar from '@/components/Navbar'
-import Temperature from '@/components/Temperature'
+import React from "react";
+import Navbar from "@/components/Navbar";
+import Temperature from "@/components/Temperature";
+import AirPollutation from "@/components/AirPollutation";
 
 const Main = () => {
   return (
-    <main className='mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto'>
-        <Navbar />
-        <div className='pb-4 flex flex-col gap-4 md:flex-row'>
-            <div className='flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]'>
-             <Temperature />
-            </div>
-            <div className='flex flex-col'></div>
+    <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
+      <Navbar />
+      <div className="pb-4 flex flex-col gap-4 md:flex-row">
+        <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
+          <Temperature />
         </div>
+        <div className="flex flex-col">
+          <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+              <AirPollutation />
+          </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
