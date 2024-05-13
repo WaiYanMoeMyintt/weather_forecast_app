90d1cbc5f1482425351d2f53dc10aeed33f3747b
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-05-13 16:16:12 +0630

    Add weather pollution api

---
+++ app/api/pollution/route.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(req:NextRequest){
+    try {
+        const lat = 40.730610;
+        const lon = -73.935242;
+        const api = "29a6376f8bab0bb516a8f48864e7d3f5";
+        const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api}`;
+
+        const getAPI = await axios.get(url);
+        return NextResponse.json(getAPI.data);
+    }
+    catch(error){
+        return `You cant get api data at right now ${error}`;
+    }
+}
